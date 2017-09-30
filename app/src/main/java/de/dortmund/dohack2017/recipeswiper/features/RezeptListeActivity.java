31498718b0b1346698e162fb66431e1281d293f0
DerMilchmann <jakob.rhb@googlemail.com>
2017-10-01 01:20:33 +0200

    Rezept Liste

--- app/src/main/java/de/dortmund/dohack2017/recipeswiper/features/RezeptListeActivity.java
+++ app/src/main/java/de/dortmund/dohack2017/recipeswiper/features/RezeptListeActivity.java
@@ -6,7 +6,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.dortmund.dohack2017.recipeswiper.R;
 import de.dortmund.dohack2017.recipeswiper.models.Rezept;
@@ -14,6 +21,8 @@ import de.dortmund.dohack2017.recipeswiper.models.Rezept;
 public class RezeptListeActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private List<Rezept> rezepte;
+    private ListView listView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -40,6 +49,29 @@ public class RezeptListeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rezept_liste);
+
+        Intent myIntent = getIntent();
+        rezepte = (List<Rezept>) myIntent.getSerializableExtra("rezepteJa");
+
+        listView  = (ListView) findViewById(R.id.list);
+        List<String> values = new ArrayList<String>();
+        for(Rezept r: rezepte)
+        {
+            values.add(r.getName());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, android.R.id.text1,values);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemPosition = position;
+                Rezept itemValue = (Rezept) listView.getItemAtPosition(position);
+                singleRezeptActivity(itemValue);
+            }
+        });
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
