ebbeb7e06eb8e439f5ab0fc2670987508a141a6c
DerMilchmann <jakob.rhb@googlemail.com>
2017-10-01 02:36:50 +0200

    Rezept Anzeige

--- app/src/main/java/de/dortmund/dohack2017/recipeswiper/features/RezeptActivity.java
+++ app/src/main/java/de/dortmund/dohack2017/recipeswiper/features/RezeptActivity.java
@@ -1,17 +1,21 @@
 package de.dortmund.dohack2017.recipeswiper.features;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import de.dortmund.dohack2017.recipeswiper.R;
+import de.dortmund.dohack2017.recipeswiper.models.Rezept;
 
 public class RezeptActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    Rezept r;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -38,6 +42,12 @@ public class RezeptActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rezept);
+
+        Intent myIntent = getIntent();
+        r = (Rezept) myIntent.getSerializableExtra("rezept");
+
+        ImageView iv = (ImageView)findViewById(R.id.bild);
+        iv.setImageResource(r.getImageResourceId());
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
